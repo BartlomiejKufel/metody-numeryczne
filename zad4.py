ca35c5963bb9f6960a720a4d7e5536465b9245c0
Bartłomiej Kufel <joloczok123@gmail.com>
2025-10-27 14:53:07 +0100

    Dodanie 5 zadania

--- zad4.py
+++ zad4.py
@@ -3,9 +3,11 @@ import math
 def function(x):
     return math.sin(x)-(x/2)
 
+#pochodna 1 stopnia
 def derivative1_function(x):
     return math.cos(x)-(1/2)
 
+#pochodna 2 stopnia
 def derivative2_function(x):
     return -math.cos(x)
 
@@ -14,18 +16,20 @@ section = [(math.pi/2),math.pi]
 n=0
 x=0
 
-#wybranie, która połowa będzie x0
-if function(section[0]) * derivative2_function(section[0]) > 0:
-    x = section[0]
-elif function(section[1]) * derivative1_function(section[1]) > 0:
-    x = section[1]
+if function(section[0]) * function(section[1]) < 0:
 
-print(f"x{n} = {x}")
-while True:
-    xn = x - (function(x)/derivative1_function(x)) #obliczenie xn+1
-    x = xn #ustawienie nowego xn
-    n+=1
-    print(f"x{n} = {xn}")
+    #wybranie, która połowa będzie x0
+    if function(section[0]) * derivative2_function(section[0]) > 0:
+        x = section[0]
+    elif function(section[1]) * derivative1_function(section[1]) > 0:
+        x = section[1]
 
-    if abs(function(xn)) <= E: #warunek wyjścia
-        break
+    print(f"x{n} = {x}")
+    while True:
+        xn = x - (function(x)/derivative1_function(x)) #obliczenie xn+1
+        x = xn #ustawienie nowego xn
+        n+=1
+        print(f"x{n} = {xn}")
+
+        if abs(function(xn)) <= E: #warunek wyjścia
+            break
