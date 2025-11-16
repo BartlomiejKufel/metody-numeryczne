b70feeb138f00fc25173a29ab41df07ad9b14699
Bartłomiej Kufel <joloczok123@gmail.com>
2025-11-16 14:48:48 +0100

    Dodanie 9 zadania

--- zad9.py
+++ zad9.py
@@ -1,4 +1,15 @@
-from sympy import Symbol, exp, sin
+# linki do wzorów
+# https://chatgpt.com/s/t_6919d5e1c79c8191b7a381461cad3463
+# https://pl.wikipedia.org/wiki/Ca%C5%82kowanie_numeryczne <- link do błędu
+from sympy import Symbol, exp, sin, diff
+
+def define_parts(n, a, b):
+    result = []
+
+    for i in range(0, n + 1):
+        result.append(a + (i / n) * (b - a))
+
+    return result
 
 x = Symbol('x')
 
@@ -6,7 +17,33 @@ x = Symbol('x')
 a = -3
 b = 1
 f = sin(x) * exp(-3*x) + x ** 3 # exp = E do potęgi
-print(f)
 
-n = 5
-h = (b - a)/(2 * n)
+
+n = 4 # n musi być parzyste
+h = (b - a)/ n
+
+parts = define_parts(n, a, b)
+
+s_even = 0 # suma parzystych indexów
+s_odd = 0 # suma nieparzystych indeksów
+for i in range(1, n):
+    if i % 2 == 0:
+        s_even += f.subs(x, parts[i])
+    else:
+        s_odd += f.subs(x, parts[i])
+
+s = f.subs(x, a) + f.subs(x, b) + (2 * s_even) + (4 * s_odd)
+s *= h/3
+
+print(f"całka[{a},{b}] = {f} * dx")
+print(f"całka[{a},{b}] ≈ {float(s)}")
+
+# to jest do błędu
+f1 = diff(f,x)
+f2 = diff(f1,x)
+f3 = diff(f2,x)
+f4 = diff(f3,x)
+max_f4 = max(abs(f4.subs(x,a)), abs(f4.subs(x,b)))
+R = ((b-a)*h**4)/180 * max_f4
+print(f"Błąd ≈ {float(R)}")
+
