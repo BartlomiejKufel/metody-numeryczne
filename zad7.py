1f20c1da69a75b9bdacbcd9b14a13031d140957f
Bartłomiej Kufel <joloczok123@gmail.com>
2025-11-16 13:28:41 +0100

    Dodanie liczenia błędu w 7 zadaniu

--- zad7.py
+++ zad7.py
@@ -1,6 +1,7 @@
 #link do wzorów
 #https://eduinf.waw.pl/inf/alg/004_int/0003.php
-from sympy import Symbol, sqrt
+#https://pl.wikipedia.org/wiki/Ca%C5%82kowanie_numeryczne <- tutaj jest wzór na błąd
+from sympy import Symbol, sqrt, diff
 
 
 def define_parts(n, a, b):
@@ -29,3 +30,10 @@ s += (f.subs(x, a) + f.subs(x, b))/2
 s *= h
 print(f"całka[{a},{b}] = {f} * dx")
 print(f"całka[{a},{b}] ≈ {float(s)}")
+
+# Liczenie błędu
+f1 = diff(f,x)
+f2 = diff(f1,x)
+max_f2 = max(abs(f2.subs(x,a)), abs(f2.subs(x,b)))
+R = (b-a)**3/(12*n**2) * max_f2
+print(f"Błąd ≈ {float(R)}")
